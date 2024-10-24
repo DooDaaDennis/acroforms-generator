96e844054a773331a66afba2b31515a54a544f67
DooDaaDennis <dennis.applebaum@btinternet.com>
2024-10-24 23:15:32 +0100

    logging innards

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const pdfExtract = new PDFExtract();
 const options = {}; /* see below */
 pdfExtract.extract("UV20422-1.pdf", options, (err, data) => {
   if (err) return console.log(err);
-  console.log(data);
-  console.log("---------------------------------------------");
-  console.log(data.pages[1]);
+  // console.log(data); //Print all doc to console
+  // console.log("---------------------------------------------");
+  // console.log(data.pages[1]); //Print page two to console - pages stored in pages array
+
+  //content stored as objects in the 'content' array, a property of each page object
+  const myPages = data.pages;
+  //console.log(myPages);
+  myPages.forEach((element) => {
+    console.log(element.pageInfo.num);
+    console.log(element.content);
+  });
 });
