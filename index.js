11a2e34ed2dc40adcd67a60fc3679586bac2560d
DooDaaDennis <dennis.applebaum@btinternet.com>
2024-10-25 02:58:46 +0100

    cleaned

--- index.js
+++ index.js
@@ -3,34 +3,16 @@ const pdfExtract = new PDFExtract();
 const options = {}; /* see below */
 pdfExtract.extract("UV20422-1.pdf", options, (err, data) => {
   if (err) return console.log(err);
-  //console.log(data); //Print all doc to console
-  // console.log("---------------------------------------------");
-  //console.log(data.pages[17]); //Print page 18 to console - pages stored in pages array
-  //console.log(data.pages);
   //content stored as objects in the 'content' array, a property of each page object
   const myPages = data.pages;
-  //console.log(myPages);
-
-  // const page1Text = myPages[1].content;
-  // console.log(page1Text);
-  //myPages.forEach((element, index) => (element.content.pageNumber = index));
+  var textOnly = [];
 
   // Add page numbers to each text snippet - loop through each page, and within each page loop through the text snippets and add the array index
   myPages.forEach((element, index) => {
     element.content.forEach((textObject) => (textObject.pageNumber = index));
+    //push all textObjects to a single array
+    element.content.forEach((textObject) => textOnly.push(textObject));
   });
 
-  //Gives an array, with each page as an entry.
-  const myPagesContent = myPages.map((page) => page.content);
-
-  console.log(myPagesContent);
-
-  // myPagesContent.forEach((element, index) => {});
-
-  // myPages.forEach((element, index) => {
-  //   //console.log(element.content);
-  //   // console.log(pageTextArray);
-  //   // pageTextArray.find(element, index) => {
-  //   // }
-  // });
+  console.log(textOnly);
 });
