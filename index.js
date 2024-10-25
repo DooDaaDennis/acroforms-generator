741d4cab26a785a009e45533e539501188ae83bf
DooDaaDennis <dennis.applebaum@btinternet.com>
2024-10-25 03:40:05 +0100

    global text var

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const PDFExtract = require("pdf.js-extract").PDFExtract;
 const pdfExtract = new PDFExtract();
 const options = { disableCombineTextItems: false }; //do not attempt to combine same line text items
+var textOnly = [];
 
 function extractText(pdf) {
   pdfExtract.extract(pdf, options, (err, data) => {
     if (err) return console.log(err);
     //content stored as objects in the 'content' array, a property of each page object
     const myPages = data.pages;
-    var textOnly = [];
 
     // Add page numbers to each text snippet - loop through each page, and within each page loop through the text snippets and add the array index
     // Produces textOnly array of text snippets
@@ -28,3 +28,4 @@ function extractText(pdf) {
 
 //Call extract text function on a PDF
 extractText("UV20422-1.pdf");
+setTimeout(() => console.log(textOnly), 5); //have to do this or it won't log
