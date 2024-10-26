410456f9b687d015cb69eac7cca7becf25acdb34
DooDaaDennis <dennis.applebaum@btinternet.com>
2024-10-26 11:52:39 +0100

    Return textOnly with promise

--- index.js
+++ index.js
@@ -4,28 +4,37 @@ const options = { disableCombineTextItems: false }; //do not attempt to combine 
 var textOnly = [];
 
 function extractText(pdf) {
-  pdfExtract.extract(pdf, options, (err, data) => {
-    if (err) return console.log(err);
-    //content stored as objects in the 'content' array, a property of each page object
-    const myPages = data.pages;
-
-    // Add page numbers to each text snippet - loop through each page, and within each page loop through the text snippets and add the array index
-    // Produces textOnly array of text snippets
-    myPages.forEach((element, index) => {
-      element.content.forEach((textObject) => (textObject.pageNumber = index));
-      element.content.forEach((textObject) => textOnly.push(textObject));
+  return new Promise((resolve, reject) => {
+    pdfExtract.extract(pdf, options, (err, data) => {
+      if (err) return console.log(err);
+      resolve(data);
     });
-    //console.log(textOnly);
-
-    //Check if there are any anomalies - i.e. more than 8 properties in each object.
-    //Produces anomalies array of text snippets
-    const anomalies = textOnly.filter(
-      (textObject) => Object.keys(textObject).length > 8
-    );
-    //console.log(anomalies);
   });
 }
 
-//Call extract text function on a PDF
-extractText("UV20422-1.pdf");
-setTimeout(() => console.log(textOnly), 5); //have to do this or it won't log
+extractText("UV20422-1.pdf").then((data) => {
+  const myData = data;
+  const myPages = data.pages;
+  myPages.forEach((element, index) => {
+    element.content.forEach((textObject) => (textObject.pageNumber = index));
+    element.content.forEach((textObject) => textOnly.push(textObject));
+  });
+  console.log(textOnly);
+});
+// //content stored as objects in the 'content' array, a property of each page object
+
+// Add page numbers to each text snippet - loop through each page, and within each page loop through the text snippets and add the array index
+// Produces textOnly array of text snippets
+
+//console.log(textOnly);
+
+//Check if there are any anomalies - i.e. more than 8 properties in each object.
+//Produces anomalies array of text snippets
+const anomalies = textOnly.filter(
+  (textObject) => Object.keys(textObject).length > 8
+);
+//console.log(anomalies);
+
+// //Call extract text function on a PDF
+
+// setTimeout(() => console.log(textOnly), 5); //have to do this or it won't log
