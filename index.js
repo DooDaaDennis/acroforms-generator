4f2ccba5328b0fd1793d57806fe0e4c124f2b4df
DooDaaDennis <dennis.applebaum@btinternet.com>
2024-10-26 12:36:38 +0100

    filter by specific text string

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const pdfExtract = new PDFExtract();
 const options = { disableCombineTextItems: false }; //do not attempt to combine same line text items
 var textOnly = [];
 
-function extractText(pdf) {
+//extracts data from the pdf
+function extractData(pdf) {
   return new Promise((resolve, reject) => {
     pdfExtract.extract(pdf, options, (err, data) => {
       if (err) return console.log(err);
@@ -12,16 +13,25 @@ function extractText(pdf) {
   });
 }
 
-//Call extract text function on a PDF
-extractText("UV20422-1.pdf")
-  .then((data) => {
-    const myData = data;
-    const myPages = data.pages;
-    myPages.forEach((element, index) => {
-      element.content.forEach((textObject) => (textObject.pageNumber = index)); //Add page numbers to each text snippet - loop through each page, and within each page loop through the text snippets and add the array index
-      element.content.forEach((textObject) => textOnly.push(textObject)); //Produces textOnly array of text snippets
-    });
-  })
-  .then(() => {
-    console.log(textOnly);
+//takes the data and extracts only the text content, as well as annotating these with the page number
+function extractText(data) {
+  const myData = data;
+  const myPages = data.pages;
+  myPages.forEach((element, index) => {
+    element.content.forEach((textObject) => (textObject.pageNumber = index)); //Add page numbers to each text snippet - loop through each page, and within each page loop through the text snippets and add the array index
+    element.content.forEach((textObject) => textOnly.push(textObject)); //Produces textOnly array of text snippets
   });
+}
+
+function searchText() {
+  const filteredText = textOnly.filter((text) => text.str === "GLH");
+  console.log(filteredText);
+}
+
+//Call extract text function on a PDF
+extractData("UV20422-1.pdf") //
+  .then(extractText)
+  .then(searchText);
+// .then(() => {
+//   console.log(textOnly);
+// });
